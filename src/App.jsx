19aa4d455ddc9d8c99d3e01a2507c01ea32c9858
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-20 17:25:39 -0500

    Imported and added Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 
 function App() {
   return (
     <>
+      <Header />
       <Footer />
     </>
   )
