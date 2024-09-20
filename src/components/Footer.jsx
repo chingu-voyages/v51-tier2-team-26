24d722ca68d7b9f94ab47438ec7f65c7c04a4ea1
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-20 16:34:38 -0500

    Built Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,59 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid2';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+const github = <FontAwesomeIcon icon={faGithub} />
+const linkedin = <FontAwesomeIcon icon={faLinkedin} />
+const twitter = <FontAwesomeIcon icon={faTwitter} />
+
+const linkSX = {
+    "&:hover":{color:'#76C57F'},
+    mx: 2,
+    fontWeight: "regular"
+}
+
+function Copyright() {
+  return (
+    <Typography variant="body2" sx={{ color: 'white', mt: 0.5 }}>
+      {'Copyright ©'}
+      &nbsp;
+      {new Date().getFullYear()}
+      &nbsp;
+      {'| Divvy Up'}
+    </Typography>
+  );
+}
+
+export default function Footer() {
+    return(
+        <Box component='section' sx={{ backgroundColor: '#0F861C', color: 'white', py: 2 }}>
+            <Grid container justifyContent='center'>
+                <Grid size={3}>
+                    <Typography variant='h6'>GitHub Repository</Typography>
+                    <Box sx={{ pt: 0.5 }}>
+                        <Link href='https://github.com/chingu-voyages/v51-tier2-team-26' target='_blank' color='inherit' variant='h3' sx={linkSX}>{github}</Link>
+                    </Box>
+                </Grid>
+                <Grid size={3}>
+                    <Typography variant='h6'>Terms and Privacy</Typography>
+                    <Box sx={{ pt: 1.5 }}>
+                        <Link href='https://www.chingu.io/privacy' target='_blank' color='inherit' variant='b3' underline='hover' sx={linkSX}>Privacy Terms</Link>
+                    </Box>
+                </Grid> 
+                <Grid size={3}>
+                    <Typography variant='h6'>Social Media</Typography>
+                    <Box sx={{ pt: 0.5 }}>
+                        <Link href='https://www.linkedin.com/company/chingu-os/' target='_blank' color='inherit' variant='h3' sx={linkSX}>{linkedin}</Link>
+                        <Link href='https://twitter.com/ChinguCollabs' target='_blank' color='inherit' variant='h3' sx={linkSX}>{twitter}</Link>
+                    </Box>
+                </Grid>
+            </Grid>
+            <Copyright />
+        </Box>
+    )
+}
