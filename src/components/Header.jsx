905c50e515838e2c512238a0246ade4ea8bf85be
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-22 18:58:45 -0500

    Made logo and 'Divvy Up' smaller

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,34 +5,33 @@ import logo from '../img/divvy-up-logo.png';
 export default function Header() {
     return(
         <Box component='section' display='flex' justifyContent='space-between' alignItems='center' sx={{ backgroundColor: 'whitesmoke', color: 'black', px:5, py:3 }}>
-            <img src={logo} alt='Divvy Up logo' height='140px' width='auto' />
+            <img src={logo} alt='Divvy Up logo' height='100px' width='auto' />
             <Box display='flex'>
                 <Box component='span' 
                     sx={{ 
                         backgroundColor: '#0F861C',
-                        border: 3,
+                        border: 2,
                         borderRight: 0,
                         borderTopLeftRadius: 20,
                         borderBottomLeftRadius: 20,
-                        pr:1,
+                        pr:0.75,
                         pl: 1.5,
-                        pb: 1,
                     }}
                 >
-                    <Typography variant='h1' sx={{color:'white'}}>Divvy</Typography>
+                    <Typography variant='h2' sx={{color:'white'}}>Divvy</Typography>
                 </Box>
                 <Box component='span' 
                     sx={{ 
                         backgroundColor: '#76C57F',
-                        border: 3,
+                        border: 2,
                         borderLeft: 0,
                         borderTopRightRadius: 20,
                         borderBottomRightRadius: 20,
                         pr:1.5,
-                        pb:1,
+                        pl: 0.3
                     }}
                 >
-                    <Typography variant='h1' sx={{color:'white'}}>Up</Typography>
+                    <Typography variant='h2' sx={{color:'white'}}>Up</Typography>
                 </Box>
             </Box>
         </Box>
