b106d6d4563a0677e4507efdd611014a828c86a5
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-22 14:11:48 -0500

    Styling changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,9 +4,9 @@ import Button from '@mui/material/Button';
 
 export default function Main({ groups }) {
     return(
-        <Box sx={{ backgroundColor:'white', width: 1, height: 600, px: 6, py: 4, boxSizing: 'border-box' }}>
+        <Box sx={{ backgroundColor:'whitesmoke', width: 1, height: 600, px: 6, py: 4, boxSizing: 'border-box' }}>
             <Button variant='contained'>Add New Group</Button>
-            {groups.map((group, index) => (
+            {groups.map((group) => (
                 <GroupDisplay key={group.id} group={group} />
             ))}
         </Box>
