4d95d285f4fc6d210f5e5968fb4aecbe3d8a03c6
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-23 14:27:41 -0500

    Cleaned up formatting and added more comments

--- src/components/GroupCard.jsx
+++ src/components/GroupCard.jsx
@@ -5,33 +5,64 @@ import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 export default function GroupCard() {
     const editIcon = <FontAwesomeIcon icon={faPenToSquare} />;
-    // const groupMembers = [...group.groupMembers];
-    // console.log(groupMembers);
     
     return(
-        <Box display='flex' alignItems='center' sx={{ "&:hover":{border: '3px solid #0F861C'}, color: 'black', backgroundColor:'white', width: 1, height: 150, mt: 2, p:2, boxSizing: 'border-box', border:'1px solid gray', borderRadius: 4 }}>
-            <Box sx={{ height: 115, width: 115, backgroundColor: '#0F861C', borderRadius: 3 }}>
-                <Typography variant='h1' sx={{ color: 'white', textAlign:'center' }}>T</Typography>
-            </Box>
-            <Box sx={{ width: 1, height: 1, ml: 2 }}>
-                <Box display='flex' justifyContent='space-between' alignItems='baseline' sx={{ mb: 1 }}>
-                    <Box display='flex' alignItems='baseline'>
-                        <Box sx={{ mr: 1 }}>
-                            <Typography variant='h5'>Test4</Typography>
-                        </Box>
-                        <Box>
-                            <Typography>{editIcon}</Typography>
-                        </Box>
-                        <Box sx={{ color:'gray', fontSize:'0.8em', fontStyle:'italic' }}>
-                            #004
-                        </Box>
-                    </Box>
-                    <Box sx={{ backgroundColor:'#76C57F', px: 1, py:1/2, borderRadius: 3, fontWeight: 'bold' }}>
-                        $200
-                    </Box>
+        <Box> {/* Parent Container */}
+            {/* Group Details */}
+            <Box display='flex' alignItems='center' 
+                sx={{ 
+                    color: 'black',
+                    backgroundColor:'white',
+                    width: 0.9,
+                    height: 150,
+                    mt: 2,
+                    p:2,
+                    boxSizing: 'border-box',
+                    border:'1px solid gray',
+                    borderRadius: 4
+                }}
+            >
+                {/* Group Image */}
+                <Box 
+                    sx={{ 
+                        height: 115,
+                        width: 115,
+                        backgroundColor: '#0F861C',
+                        borderRadius: 3 
+                    }}
+                >
+                    <Typography variant='h1' sx={{ color: 'white', textAlign:'center' }}>T</Typography>
                 </Box>
-                <Box>Test Description</Box>
+                {/* End Group Image */}
+                {/* Group Text Fields */}
+                <Box 
+                    sx={{
+                        width: 1,
+                        height: 1,
+                        ml: 2 
+                    }}
+                >
+                    <Box display='flex' justifyContent='space-between' alignItems='baseline' sx={{ mb: 1 }}>
+                        <Box display='flex' alignItems='baseline'>
+                            <Box>
+                                <Typography variant='h5'>Test4</Typography>
+                            </Box>
+                            <Box sx={{ mx: 1.1 }}>
+                                <Typography sx={{ fontSize: '1.7em', color:'#FB8500' }}>{editIcon}</Typography>
+                            </Box>
+                            <Box sx={{ color:'gray', fontSize:'0.8em', fontStyle:'italic' }}>
+                                #004
+                            </Box>
+                        </Box>
+                        <Box sx={{ backgroundColor:'#76C57F', px: 1, py:1/2, borderRadius: 3, fontWeight: 'bold' }}>
+                            $200
+                        </Box>
+                    </Box>
+                    <Box>Test Description</Box>
+                </Box>
+                {/* End Group Text Fields */}
             </Box>
+            {/* End Group Details */}
         </Box>
     )
 }
