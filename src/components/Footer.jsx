9a08e0f86af19e1e64fedbd1819af2773455ac7c
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-22 23:58:29 -0500

    Decreased overall size of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const twitter = <FontAwesomeIcon icon={faTwitter} />
 
 const linkSX = {
     "&:hover":{color:'#fb8500'},
-    mx: 2,
+    mx: 1,
     fontWeight: "regular"
 }
 
@@ -31,25 +31,25 @@ function Copyright() {
 
 export default function Footer() {
     return(
-        <Box component='section' sx={{ textAlign: 'center', backgroundColor: 'whitesmoke', color: 'black', py: 2 }}>
+        <Box component='section' sx={{ textAlign: 'center', backgroundColor: 'whitesmoke', color: 'black', pt: 4, pb: 2 }}>
             <Grid container justifyContent='center'>
-                <Grid size={3}>
-                    <Typography variant='h6'>GitHub Repository</Typography>
+                <Grid size={1}>
+                    <Typography variant='h7'>GitHub Repository</Typography>
                     <Box sx={{ pt: 0.5 }}>
-                        <Link href='https://github.com/chingu-voyages/v51-tier2-team-26' target='_blank' color='inherit' variant='h3' sx={linkSX}>{github}</Link>
+                        <Link href='https://github.com/chingu-voyages/v51-tier2-team-26' target='_blank' color='inherit' variant='h4' sx={linkSX}>{github}</Link>
                     </Box>
                 </Grid>
-                <Grid size={3}>
-                    <Typography variant='h6'>Terms and Privacy</Typography>
-                    <Box sx={{ pt: 1.5 }}>
-                        <Link href='https://www.chingu.io/privacy' target='_blank' color='inherit' variant='b3' underline='hover' sx={linkSX}>Privacy Terms</Link>
+                <Grid size={1} sx={{ mx: 2 }}>
+                    <Typography variant='h7'>Terms and Privacy</Typography>
+                    <Box sx={{ pt: 0.75 }}>
+                        <Link href='https://www.chingu.io/privacy' target='_blank' color='inherit' underline='hover' fontSize='0.9em' sx={linkSX}>Privacy Terms</Link>
                     </Box>
                 </Grid> 
-                <Grid size={3}>
-                    <Typography variant='h6'>Social Media</Typography>
+                <Grid size={1}>
+                    <Typography variant='h7'>Social Media</Typography>
                     <Box sx={{ pt: 0.5 }}>
-                        <Link href='https://www.linkedin.com/company/chingu-os/' target='_blank' color='inherit' variant='h3' sx={linkSX}>{linkedin}</Link>
-                        <Link href='https://twitter.com/ChinguCollabs' target='_blank' color='inherit' variant='h3' sx={linkSX}>{twitter}</Link>
+                        <Link href='https://www.linkedin.com/company/chingu-os/' target='_blank' color='inherit' variant='h4' sx={linkSX}>{linkedin}</Link>
+                        <Link href='https://twitter.com/ChinguCollabs' target='_blank' color='inherit' variant='h4' sx={linkSX}>{twitter}</Link>
                     </Box>
                 </Grid>
             </Grid>
