2390536db8afba776aae6585c6b9d202ea13c77a
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-22 19:22:30 -0500

    Changed colors for the group image and money owed Boxes, tweaked height for the group image Box

--- src/components/GroupDisplay.jsx
+++ src/components/GroupDisplay.jsx
@@ -8,7 +8,7 @@ export default function GroupDisplay( { group }) {
     
     return(
         <Box display='flex' alignItems='center' sx={{ "&:hover":{border: '2px solid #fb8500'}, color: 'black', backgroundColor:'white', width: 1, height: 150, mt: 2, p:2, boxSizing: 'border-box', border:'1px solid gray', borderRadius: 4 }}>
-            <Box sx={{ height: 115, width: 115, backgroundColor: '#0F861C', borderRadius: 3 }}>
+            <Box sx={{ height: 105, width: 115, backgroundColor: '#76C57F', borderRadius: 3, }}>
                 <Typography variant='h1' sx={{ color: 'white', textAlign:'center' }}>{group.groupName[0]}</Typography>
             </Box>
             <Box sx={{ width: 1, height: 1, ml: 2 }}>
@@ -21,7 +21,7 @@ export default function GroupDisplay( { group }) {
                             #{group.id < 10 ? '00'+group.id : '0'+group.id}
                         </Box>
                     </Box>
-                    <Box sx={{ backgroundColor:'#76C57F', px: 1, py:1/2, borderRadius: 3, fontWeight: 'bold' }}>
+                    <Box sx={{ backgroundColor:'#ffa347', px: 1, py:1/2, borderRadius: 3, fontWeight: 'bold' }}>
                         ${group.moneyOwed}
                     </Box>
                 </Box>
