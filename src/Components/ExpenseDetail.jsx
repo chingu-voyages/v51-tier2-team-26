cc1c2f3e242ae922ed7f256deaa011a7d1364048
hectorgarcia07 <csharp12321@gmail.com>
2024-09-23 21:07:50 -0500

    finished basic layout to displaying data

--- src/Components/ExpenseDetail.jsx
+++ src/Components/ExpenseDetail.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid2, Typography } from '@mui/material'
+import { Box, Grid2, TableContainer, Typography,TableHead,TableCell,TableRow, TableBody, Paper, Table } from '@mui/material'
 import receipt from '../img/receipt.png'
 
 const propsObj = {
@@ -17,24 +17,57 @@ const propsObj = {
 }
 
 export default function ExpenseDetail() {
+  function getName(acc, curr){
+    return acc + curr.name + ', '
+  }
   return (
     <Box sx={{ flexGrow: 1, textAlign: 'start' }}>
       <Grid2 container spacing={2} >
         <Grid2 container size={12} sx={{  pb: 1, borderBottom: '1px solid black' }} >
-          <Grid2 size={8}>
+          <Grid2 size={8}  display={"flex"} flexDirection={'column'} justifyContent={'space-between'}>
             <Typography variant='h3'>{propsObj.groupName}</Typography> 
-            <Typography variant='h6'>ID: {propsObj.groupID}</Typography>
+            <Typography variant='h4'>ID: {propsObj.groupID}</Typography>
           </Grid2>
-          <Grid2 size={4} sx={{textAlign: 'end'}}>
+          <Grid2  size={4} sx={{textAlign: 'end'}} display={"flex"} flexDirection={'column'} justifyContent={'space-between'}>
             <Typography>Created on: {propsObj.date}</Typography> 
+
+            <Typography>Members: {propsObj.participants.reduce(getName, '').slice(0, -2)}</Typography> 
           </Grid2>          
         </Grid2>
 
         <Box>
           {propsObj.description}
         </Box>
-
+        
+        <Box>
+          <Typography variant='h5'>Members:</Typography>
+          <TableContainer component={Paper} sx={{ maxWidth: 300 }}>
+          <Table aria-label="simple table">
+            <TableHead>
+                <TableRow >
+                  <TableCell >Name</TableCell>
+                  <TableCell align="right">Contribution %</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {propsObj.participants.map( (data) => (
+                  <TableRow
+                    key={data.name}
+                  >
+                    <TableCell>
+                    {data.name}
+                  </TableCell>
+                  <TableCell align="right">
+                    {data.percent}
+                  </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+          </Table>
+          </TableContainer>
+        </Box>
       </Grid2>
     </Box>
   )
 }
+
