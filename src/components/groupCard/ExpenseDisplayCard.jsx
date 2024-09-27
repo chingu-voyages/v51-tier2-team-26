04ca78d49f822c1b5e4b4785cc5d93a57e66e05a
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-26 19:54:17 -0500

    Initial styling of expense displays finished

--- src/components/groupCard/ExpenseDisplayCard.jsx
+++ src/components/groupCard/ExpenseDisplayCard.jsx
@@ -1,10 +1,23 @@
 import Grid from '@mui/material/Grid2';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 export default function ExpenseDisplayCard( {expense} ) {
-        return (
-            <Grid size={3}>
-                <Typography>{expense.ExpenseName}</Typography>
-            </Grid>
+    const categories = [...expense.categories]
+
+    return (
+        <Grid size={3} sx={{
+            my:1,
+            px:2,
+            py:1,
+            border: '1px solid grey',
+            borderRadius: 3
+        }}>
+            <Typography variant='h7' fontWeight='bold'>{expense.expenseName}</Typography>
+            <Typography>
+                {categories}
+            </Typography>
+            <Typography variant='b1'>{expense.description}</Typography>
+        </Grid>
     )
 }
