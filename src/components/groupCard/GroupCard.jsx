39347696ec833c663af620de125c832ea16c7d42
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-26 20:27:49 -0500

    Changed edit icon color and improved readability of code

--- src/components/groupCard/GroupCard.jsx
+++ src/components/groupCard/GroupCard.jsx
@@ -10,47 +10,42 @@ export default function GroupCard() {
     
     return(
         // Parent Container  
-        <Box 
-            sx={{ 
-                color: 'black',
-                backgroundColor:'white',
-                width: 0.9,
-                height: 'auto',
-                mt: 2,
-                p:2,
-                boxSizing: 'border-box',
-                border:'1px solid gray',
-                borderRadius: 4
-            }}>
+        <Box sx={{ 
+            color: 'black',
+            backgroundColor:'white',
+            width: 0.9,
+            height: 'auto',
+            mt: 2,
+            p:2,
+            boxSizing: 'border-box',
+            border:'1px solid gray',
+            borderRadius: 4
+        }}>
             {/**** Group Info ****/}
             <Box display='flex' alignItems='center'>
                 {/*** Group Image ***/}
-                <Box 
-                    sx={{ 
-                        height: 105,
-                        width: 115,
-                        backgroundColor: '#76C57F',
-                        borderRadius: 3 
-                    }}
-                >
+                <Box sx={{ 
+                    height: 105,
+                    width: 115,
+                    backgroundColor: '#76C57F',
+                    borderRadius: 3 
+                }}>
                     <Typography variant='h1' sx={{ color: 'white', textAlign:'center' }}>T</Typography>
                 </Box>
                 {/* End Group Image */}
                 {/*** Group Details ***/}
-                <Box 
-                    sx={{
-                        width: 1,
-                        height: 1,
-                        ml: 2 
-                    }}
-                >
+                <Box sx={{
+                    width: 1,
+                    height: 1,
+                    ml: 2 
+                }}>
                     <Box display='flex' justifyContent='space-between' alignItems='baseline' sx={{ mb: 1 }}>
                         <Box display='flex' alignItems='baseline'>
                             <Box>
                                 <Typography variant='h5'>Test4</Typography>
                             </Box>
                             <Box sx={{ mx: 1.1 }}>
-                                <Typography sx={{ fontSize: '1.7em', color:'#FB8500' }}>{editIcon}</Typography>
+                                <Typography sx={{ fontSize: '1.7em', color:'#0F861C' }}>{editIcon}</Typography>
                             </Box>
                             <Box sx={{ color:'gray', fontSize:'0.8em', fontStyle:'italic' }}>
                                 #004
